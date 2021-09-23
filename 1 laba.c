480da6a340d638767d9e254df9c16577d85182f5
RomanKosovets <90088812+RomanKosovets@users.noreply.github.com>
2021-09-23 20:16:01 +0300

    Add files via upload

---
+++ 1 laba.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int var;
+    scanf("%d", &var);
+    printf("%d\n", var);
+    char str[0];
+    scanf("%s", str);
+    printf("%s", str);
+    return 0;
+}
