fdb8d466fc512fedd749daefc6e4601a0cd7dd0f
RomanKosovets <90088812+RomanKosovets@users.noreply.github.com>
2021-09-23 22:38:53 +0300

    Update 2 laba.c

--- 2 laba.c
+++ 2 laba.c
@@ -9,6 +9,6 @@ int main() {
     scanf("%f", &alpha);
     fun1 = cos(alpha) + cos(2 * alpha) + cos(6 * alpha) + cos(7 * alpha);
     fun2 = 4 * cos(alpha / 2) * cos((5 * alpha) / 2) * cos(4 * alpha);
-    printf("fun1 = %f, fun2 = %f\n", fun1, fun2);
+    printf("fun1 = %f\nfun2 = %f\n", fun1, fun2);
     return 0;
 }
